00702985c741599184b2381e2b18cc44538ced56
FrozenCity541 <chanmatthew787@gmail.com>
2023-04-09 11:48:55 +0800

    my teacher is good

---
+++ class3/Hw QQ.py
@@ -0,0 +1,24 @@
+# """
+# 請輸入身高:1.7
+# 請輸入體重:50
+# 你的BMI為17.301038062283737
+# """
+# h = float(input("身高="))
+# w = float(input("體重="))
+# bmi = w / h**2
+# print("你的BMI為" + str(bmi))
+
+# if 16.4 <= bmi <= 21.5:
+#     print("normal")
+# elif bmi > 21.5:
+#     print("u are so fat")
+# elif bmi < 16.4:
+#     print(" u eat dirt")
+
+# EX:
+# 請輸入華氏溫度:60
+# 華氏溫度60.0F等於攝氏溫度15.555555555555555C
+
+F = float(input("溫度"))
+C = (F - 32) * 5 / 9
+print(f"華氏溫度{F}等於攝氏溫度{C}")
